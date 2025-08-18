4d024989dc7183965bbe202a35dc3d314fa45cdd
Sebastian Konieczny <s.konieczny3211@gmail.com>
2025-08-18 17:39:59 +0200

    Create function to fetch leagues info

--- fetcher/fetcher.py
+++ fetcher/fetcher.py
@@ -9,35 +9,38 @@ BASE_URL = 'https://fbrapi.com'
 # Codes used to describe leagues we are looking for
 COUNTRY_CODES = ["ITA", "ESP", "FRA", "GER", "ENG"]
 
-def get_league_ids(api_key: str) -> dict[str, int]:
+def get_league_info(api_key: str) -> dict[str, dict]:
     url = BASE_URL + "/leagues"
     headers = {'X-API-Key': api_key}
 
-    league_ids = {}
+    league_info = {}
 
     for country in COUNTRY_CODES:
         response = requests.get(url, params={"country_code": country}, headers=headers)
         if response.status_code == 200:
+            league_info[country] = {}
+
             # get data to describe tier 1 leagues in given countries
             league_data = response.json()["data"][0]['leagues'][0]
-            competition_name = league_data["competition_name"]
-            league_id = league_data["league_id"]
 
-            league_ids[competition_name] = league_id
+            league_info[country] = {
+                "league_id": league_data["league_id"],
+                "competition_name": league_data["competition_name"]
+            }
 
             # add a delay as stated in FBR API docs to prevent restrictions
             time.sleep(3)
 
-    return league_ids
+    return league_info
 
 def main() -> None:
     # generate unique API Key needed to scrape the data
     response = requests.post(BASE_URL + '/generate_api_key')
     FBR_API_KEY = response.json().get('api_key')
 
-    league_ids = get_league_ids(FBR_API_KEY)
+    league_ids = get_league_info(FBR_API_KEY)
 
-    print(league_ids)
+    print(json.dumps(league_ids, indent=4))
 
     # print(response.status_code)
     #
