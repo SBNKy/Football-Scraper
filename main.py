01d30b589a5bac19e7a2173bae783d8e87e08420
Sebastian Konieczny <s.konieczny3211@gmail.com>
2025-08-18 20:21:39 +0200

    Update dependencies

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import pandas as pd
+
+
 def main():
-    print("Hello from footballscraper!")
+    df = pd.read_html("https://fbref.com/en/comps/9/Premier-League-Stats",
+        attrs={"id": "results2025-202691_overall"})[0]
+    print(df.head())
+
 
 
 if __name__ == "__main__":
