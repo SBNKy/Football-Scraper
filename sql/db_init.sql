ea31e8fa4657bd17552e0ef227a0d256507669ce
Sebastian Konieczny <s.konieczny3211@gmail.com>
2025-08-18 19:19:39 +0200

    lets start from scratch

--- sql/db_init.sql
+++ sql/db_init.sql
@@ -2,9 +2,22 @@ DROP TABLE IF EXISTS players;
 DROP TABLE IF EXISTS teams;
 
 CREATE TABLE teams (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    content TEXT
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    competition_name VARCHAR(50) NOT NULL,
+    position INT NOT NULL,
+    team_name VARCHAR(50) NOT NULL,
+    matches_played INT NOT NULL,
+    wins INT NOT NULL,
+    draws INT NOT NULL,
+    losses INT NOT NULL,
+    goals_scored INT NOT NULL,
+    goals_conceded INT NOT NULL,
+    goals_diff INT GENERATED ALWAYS AS (goals_scored - goals_conceded) STORED,
+    points INT NOT NULL,
+    expected_goals NUMERIC(3, 2) NOT NULL,
+    expected_goals_conceded NUMERIC(3, 2) NOT NULL,
+    expected_goals_diff NUMERIC(3, 2) GENERATED ALWAYS AS (expected_goals - expected_goals_conceded) STORED,
+    expected_goals_diff_90_min NUMERIC(3, 2) NOT NULL
 );
 
 CREATE TABLE players (
