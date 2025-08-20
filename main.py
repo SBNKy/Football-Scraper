fed252c391bcdaf7352c7a4e214a1692f656b3a3
Sebastian Konieczny <s.konieczny3211@gmail.com>
2025-08-20 23:17:15 +0200

    test out pandas scraping method

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pandas as pd
+import requests
 
 
 def main():
     df = pd.read_html("https://fbref.com/en/comps/9/Premier-League-Stats",
         attrs={"id": "results2025-202691_overall"})[0]
     print(df.head())
-
-
 
 if __name__ == "__main__":
     main()
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,8 +1,27 @@
+import json
+
+import requests
+from bs4 import BeautifulSoup
+
+URLS = {
+    "Premier League": "https://fbref.com/en/comps/9/Premier-League-Stats",
+    "La Liga": "https://fbref.com/en/comps/12/La-Liga-Stats",
+    "Bundesliga": "https://fbref.com/en/comps/20/Bundesliga-Stats",
+    "Serie A": "https://fbref.com/en/comps/11/Serie-A-Stats",
+    "Ligue 1": "https://fbref.com/en/comps/13/Ligue-1-Stats"
+}
+
+def scrape_league_data() -> None:
+    pass
 
 
 def main() -> None:
-    pass
-
+    for league, url in URLS.items():
+        print(league, url)
+    response = requests.get(URLS.get("Premier League"), 'html.parser')
+    print(response.status_code)
+    print(response.text)
+    # print(json.dumps(response.json(), indent=4))
 
 if __name__ == '__main__':
     main()
