86c01b2dd75948b79e32c997e7678fd32cebc9d4
Sebastian Konieczny <s.konieczny3211@gmail.com>
2025-08-21 00:21:27 +0200

    create dataframes for every league standings

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,32 +1,43 @@
+from io import StringIO
+
 import pandas as pd
-from bs4 import BeautifulSoup
 from cloudscraper import create_scraper
-from six import StringIO
+from pandas import DataFrame
 
 LEAGUE_URLS = {
-    "Premier League": "https://fbref.com/en/comps/9/Premier-League-Stats",
-    "La Liga": "https://fbref.com/en/comps/12/La-Liga-Stats",
-    "Bundesliga": "https://fbref.com/en/comps/20/Bundesliga-Stats",
-    "Serie A": "https://fbref.com/en/comps/11/Serie-A-Stats",
-    "Ligue 1": "https://fbref.com/en/comps/13/Ligue-1-Stats"
+    "Premier League": {"url": "https://fbref.com/en/comps/9/Premier-League-Stats",
+                       "attribute_id": "results2025-202691_overall"},
+    "La Liga": {"url": "https://fbref.com/en/comps/12/La-Liga-Stats",
+                "attribute_id": "results2025-2026121_overall"},
+    "Bundesliga": {"url": "https://fbref.com/en/comps/20/Bundesliga-Stats",
+                   "attribute_id": "results2025-2026201_overall"},
+    "Serie A": {"url": "https://fbref.com/en/comps/11/Serie-A-Stats",
+                "attribute_id": "results2025-2026111_overall"},
+    "Ligue 1": {"url": "https://fbref.com/en/comps/13/Ligue-1-Stats",
+                "attribute_id": "results2025-2026131_overall"}
 }
 
-def scrape_league_data() -> None:
+def scrape_league_data() -> DataFrame:
+    df_list = []
     scraper = create_scraper()
-    response = scraper.get("https://fbref.com/en/comps/9/Premier-League-Stats").text
-    df = pd.read_html(StringIO(response), attrs={"id": "results2025-202691_overall"})[0]
 
-    # soup = BeautifulSoup(response, features="lxml")
-    # table = soup.find_all("table")
-    print(df.head())
+    for league_url, data in LEAGUE_URLS.items():
+        response = scraper.get(data["url"]).text
+        df = pd.read_html(StringIO(response), attrs={"id": data["attribute_id"]})[0]
+        if "Last 5" not in df.columns:
+            df.insert(15, "Last 5", pd.NA)
+
+        df_list.append(df)
+
+
+    return df_list
+    # print(df["Top Team Scorer"])
 
 
 
 
 def main() -> None:
     scrape_league_data()
-
-
 
 if __name__ == '__main__':
     main()
